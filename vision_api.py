550464e46dd63ab763d6bc2928ded00212335ca9
mavera <mavera@liverpool.com.mx>
2024-01-29 10:33:47 -0600

    change error message to default log

--- vision_api.py
+++ vision_api.py
@@ -38,24 +38,30 @@ class Vision:
         y devuelve la información necesaria.
         """
 
-        vertices = []
+        lrgst_object = 0
 
         # Iterar los objetos encontrados en la imagen.
-        # Por conveniencia solo se trabajará con el primer
-        # objeto encontrado.
+        # Por conveniencia para el laboratiorio
+        # sólo se trabajará con el objeto más
+        # grande encontrado en por la Cloud Vision API.
         for object_ in objects:
-            # Obtener los vertices para gráficar.
-            # Los vertices están normalizados.
+            tmp_vertices = []
+            # Obtener los vertices normalizados del objecto encontrado
             for vertex in object_.bounding_poly.normalized_vertices:
                 tmp = (vertex.x, vertex.y)
-                vertices.append(tmp)
+                tmp_vertices.append(tmp)
+            # Obtener el área del objeto
+            tpm_area = abs(tmp_vertices[0][0] - tmp_vertices[2][0]) * abs(tmp_vertices[0][1] - tmp_vertices[2][1])
+            # Conservar únicamente el objeto más grande
+            if tpm_area > lrgst_object:
+                lrgst_object = tpm_area
+                object_name = object_.name
+                object_score = object_.score
+                vertices = tmp_vertices
 
-            # Se devuelven los vértices e información
-            # adicional como el nombre del objeto que se 
-            # encontró y la probabilidad de acertividad.
-            return object_.name, object_.score * 100, vertices
+        # Regresar datos procesados de la imagen
+        return object_name, object_score * 100, vertices
 
-        return False
 
     @staticmethod
     def crop_object(image, vertices):
@@ -70,16 +76,16 @@ class Vision:
         # Obtener líneas y columnas de la imagen
         (number_of_rows, number_of_columns) = original_image.shape[:2]
         # Desnormalizar vertices
-        v1_y = round(vertices[0][0] * number_of_columns)
-        v1_x = round(vertices[0][1] * number_of_rows)
-        v2_y  = round(vertices[2][0] * number_of_columns)
-        v2_x = round(vertices[2][1] * number_of_rows)
+        v1_x = round(vertices[0][0] * number_of_columns)
+        v1_y = round(vertices[0][1] * number_of_rows)
+        v2_x  = round(vertices[2][0] * number_of_columns)
+        v2_y = round(vertices[2][1] * number_of_rows)
         # Agregar un rectángulo en la imagen para
         # encuadrar el objeto encontrado en la imagen.
         cropped_img = cv2.rectangle(
             original_image,
-            (v1_y, v1_x),
-            (v2_y, v2_x),
+            (v1_x, v1_y),
+            (v2_x, v2_y),
             (0, 255, 0),
             8
         )
